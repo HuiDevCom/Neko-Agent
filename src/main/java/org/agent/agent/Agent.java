0a397a1d46403275885233b289a9493ea1d990c7
HuiDevCom <fenghui@huidev.com>
2026-07-28 11:02:42 +0800

    初始化项目结构，添加Gradle配置文件和基本插件，创建主插件类及其配置

---
+++ src/main/java/org/agent/agent/Agent.java
@@ -0,0 +1,16 @@
+package org.agent.agent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Agent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
